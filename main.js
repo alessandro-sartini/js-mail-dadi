563c00f6407bfb5304e394ecdbaacdf26e833d8c
alessandro-sartini <ale.sartini.98@gmail.com>
2025-01-14 12:40:11 +0100

    firste ex: ok

--- main.js
+++ main.js
@@ -74,12 +74,14 @@ if (isMailPresent) {
 // Che ci sia un array da qualche parte?
 // Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 // Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// 3. si ma noi cosa vogliamo fare?
-// 4. torniamo a scrivere in italiano
-// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-// Buon lavoro e buon divertimento! :slightly_smiling_face
+//! 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+
+//! 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+
+//! 3. si ma noi cosa vogliamo fare?
+//! 4. torniamo a scrivere in italiano
+//! 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+//! Buon lavoro e buon divertimento! :slightly_smiling_face
 
 
 
